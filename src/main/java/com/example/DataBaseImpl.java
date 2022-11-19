d3965544c77c0df9ff8cf7c4bd4599ca52924661
Vaibhav Diwan <vaibhav.diwan@niit.com>
2022-11-19 10:43:35 +0530

    first commit

--- src/main/java/com/example/DataBaseImpl.java
+++ src/main/java/com/example/DataBaseImpl.java
@@ -1,2 +1,17 @@
-package com.example;public class DataBaseImpl {
+package com.example;
+
+import java.sql.Connection;
+
+public class DataBaseImpl {
+    public static void main(String[] args) {
+        DatabaseOperations databaseOperations = new DatabaseOperations();
+
+        Connection connection = databaseOperations.getDbConnection();
+        if(connection != null){
+            System.out.println("connection established");
+        }
+        else {
+            System.out.println("connection failed");
+        }
+    }
 }
--- src/main/java/com/example/DatabaseOperations.java
+++ src/main/java/com/example/DatabaseOperations.java
@@ -1,2 +1,30 @@
-package com.example;public class DatabaseOperations {
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseOperations {
+    //define method for getting connection
+    //define method for creating table
+
+    //connection
+    public Connection getDbConnection(){
+        Connection connection = null;
+        try {
+            //register driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/salesdata";
+            String user = "root";
+            String password = "root@123";
+            //get the connection using DriverManager
+            connection = DriverManager.getConnection(url, user, password);
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
